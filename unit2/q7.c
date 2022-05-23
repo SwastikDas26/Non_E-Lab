bc12fe6de8b81c8c6002cbba9c78cc000d1c82df
SwastikDas26 <91850107+SwastikDas26@users.noreply.github.com>
2022-05-23 20:51:20 +0530

    Update q7.c

--- unit2/q7.c
+++ unit2/q7.c
@@ -1,24 +1,93 @@
 #include <stdio.h>
-
-
-// Wrong Question
 int main()
 {
+
     int a_prize, b_prize, c_prize, a_age, b_age, c_age;
+
     printf("Enter ages in order\n");
+
     scanf("%d%d%d", &a_age, &b_age, &c_age);
+
     printf("Enter prizes of of each respectively\n");
+
     scanf("%d%d%d", &a_prize, &b_prize, &c_prize);
-    if(a_age > b_age && a_age > c_age)
+
+    if (a_age > b_age && a_age > c_age)
     {
-        if(a_prize > b_prize && a_prize > c_prize)
+        if (a_prize > b_prize && a_prize > c_prize)
         {
-            printf("fair");
+
+            if (b_age == c_age && b_prize == c_prize)
+            {
+                printf("fair");
+            }
+            else
+            {
+                printf("fair");
+            }
         }
+
         else
         {
             printf("unfair");
         }
     }
-    else if()
+
+    if (b_age > a_age && b_age > c_age)
+
+    {
+
+        if (b_prize > a_prize && b_prize > c_prize)
+
+        {
+
+            if (a_age == c_age && a_prize == c_prize)
+            {
+
+                printf("fair");
+            }
+
+            else
+            {
+
+                printf("fair");
+            }
+        }
+
+        else
+
+        {
+
+            printf("unfair");
+        }
+    }
+
+    if (c_age > a_age && c_age > b_age)
+
+    {
+
+        if (c_prize > a_prize && c_prize > b_prize)
+
+        {
+
+            if (b_age == a_age && b_prize == a_prize)
+            {
+
+                printf("fair");
+            }
+
+            else
+            {
+
+                printf("fair");
+            }
+        }
+
+        else
+
+        {
+
+            printf("unfair");
+        }
+    }
 }
